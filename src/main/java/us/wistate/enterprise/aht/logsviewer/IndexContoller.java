5345165a796b48cac7b546d009eed66c2df5648e
noerodriguez <noe@wisconsinmail.com>
2023-09-28 20:49:26 -0500

    first release

--- src/main/java/us/wistate/enterprise/aht/logsviewer/IndexContoller.java
+++ src/main/java/us/wistate/enterprise/aht/logsviewer/IndexContoller.java
@@ -31,7 +31,7 @@ public class IndexContoller {
 			indexHTML.append("<ul>");
 			for(int i=0; i<contents.length; i++) {
 				if(contents[i].charAt(0) != '.'){
-					indexHTML.append("<li class=\"folder\"><i class=\"bi bi-folder-fill\"></i><a href=\"/?folder="+contents[i]+"\"> " + contents[i] + "</a></li>");
+					indexHTML.append("<li class=\"folder\"><i class=\"bi bi-folder-fill\"></i><a href=\"/LogsViewer/?folder="+contents[i]+"\"> " + contents[i] + "</a></li>");
 				}
 			}
 			indexHTML.append("</ul>");
@@ -42,23 +42,23 @@ public class IndexContoller {
 				if(Arrays.asList(contents).contains(folder)) {
 					File directoryPathSelectedFolder= new File(logsDirectoryPath+"/"+folder+"/logs");
 					String contentsSelectedFolder[] = directoryPathSelectedFolder.list();
-					indexHTML.append("<br><a href=\"/\"><i class=\"bi bi-arrow-left-circle-fill\"></i></a> Available Logs for <b>"+folder+"</b>:<br>");
+					indexHTML.append("<br><a href=\"/LogsViewer/\"><i class=\"bi bi-arrow-left-circle-fill\"></i></a> Available Logs for <b>"+folder+"</b>:<br>");
 					indexHTML.append("<ul>");
 					for(int a=0; a<contentsSelectedFolder.length; a++) {
 						if(contentsSelectedFolder[a].contains(".log")) {
-							indexHTML.append("<li class=\"file\"><i class=\"bi bi-file-earmark-fill\"></i><a href=\"/?file="+folder+"/logs/"+contentsSelectedFolder[a]+"\"> " + contentsSelectedFolder[a] + "</a></li>");
+							indexHTML.append("<li class=\"file\"><i class=\"bi bi-file-earmark-fill\"></i><a href=\"/LogsViewer/?file="+folder+"/logs/"+contentsSelectedFolder[a]+"\"> " + contentsSelectedFolder[a] + "</a></li>");
 						}
 				    }
 					indexHTML.append("</ul>");
 				}else {
-					indexHTML.append("<br><a href=\"/\"><i class=\"bi bi-arrow-left-circle-fill\"></i></a> <b>This folder does not exist.</b>");
+					indexHTML.append("<br><a href=\"/LogsViewer/\"><i class=\"bi bi-arrow-left-circle-fill\"></i></a> <b>This folder does not exist.</b>");
 				}
 			}
 			if((folder =="" || folder == null)&&(file != "") ) {
 				File f = new File(logsDirectoryPath+"/"+file);
 				if(f.exists() && !f.isDirectory()) { 
 					try {
-						indexHTML.append("<br><a href=\"/?folder="+file.split("/")[0]+"\"><i class=\"bi bi-arrow-left-circle-fill\"></i> More Logs</a><br><br>");
+						indexHTML.append("<br><a href=\"/LogsViewer/?folder="+file.split("/")[0]+"\"><i class=\"bi bi-arrow-left-circle-fill\"></i> More Logs</a><br><br>");
 						indexHTML.append("File: <b>"+file.split("/")[2]+"</b><br>");
 						FileInputStream fstream = new FileInputStream(logsDirectoryPath+"/"+file);
 						DataInputStream in = new DataInputStream(fstream);
@@ -85,7 +85,7 @@ public class IndexContoller {
 						e.printStackTrace();
 					}
 				}else {
-					indexHTML.append("<br><a href=\"/\"><i class=\"bi bi-arrow-left-circle-fill\"></i></a> <b>This file does not exist.</b>");
+					indexHTML.append("<br><a href=\"/LogsViewer/\"><i class=\"bi bi-arrow-left-circle-fill\"></i></a> <b>This file does not exist.</b>");
 				}
 				
 				
